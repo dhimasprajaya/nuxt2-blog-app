782307dd8a503a39d82eb6dc0747fdecadcf79c6
Dhimas Prajaya <dhimasprajaya@gmail.com>
2023-01-01 22:34:51 +0700

    example routing folder, vuex, and navigating programmatically

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-  <Tutorial/>
+  <div>
+    <!-- Sample make send request to the server -->
+    <a href="/users">Navigate to Users page and send request to the server</a>
+    <br>
+    <br>
+    <!-- Sample using CSR -->
+    <nuxt-link to="/users">Navigate to Users page using CSR</nuxt-link>
+    <br>
+    <br>
+    <!-- Sample using Programatically-->
+    <input type="text" v-model="userId" />
+    <br>
+    <button @click="onLoadUser">Load User</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  data() {
+    return {
+      userId: "",
+    };
+  },
+  methods: {
+    onLoadUser() {
+      // Sample using Programatically
+      this.$router.push("/users/" + this.userId);
+    },
+  },
+};
 </script>
---
+++ pages/users/_id/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Single User Page, with ID: {{ $route.params.id }}</h1>
+</template>
