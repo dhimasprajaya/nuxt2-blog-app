fad54e56b8196d75ee180ed715c2ff90f1dc87e7
Dhimas Prajaya <dhimasprajaya@gmail.com>
2023-01-01 23:11:32 +0700

    add validate

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>Single User Page, with ID: {{ $route.params.id }}</h1>
+  <h1>Single User Page, with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate(data) {
+    // console.log(data);
+    return data.params.id == 1;
+  },
+};
+</script>
